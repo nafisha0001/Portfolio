d84449c8ef9ed544a1f774a50d47e4d87dcd3c03
Naisha <nafishakhan0001@gmail.com>
2024-09-07 23:58:51 +0530

    Header and aboutme

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import Header from '../components/Header'
+import './AboutMe.css'
+import image from '../Images/nafisha.jpg'
 
 function AboutMe() {
   return (
-    <Header />
+    <div className='AboutMe'>
+      <Header />
+      <div className='aboutme-content'>
+        <div className='aboutme-content__image'>
+          <img src={image} alt="Profile" />
+        </div>
+        <div className='aboutme-content__description'>
+          <h2>Hi, I am Nafisha</h2>
+          <p>I am a B tech-(CSE) final year student with a keen interest in full-stack development and Machine Learning. I enjoy solving complex problems and creating impactful projects.</p>
+          <button className='contact-button'>Contact Me</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
